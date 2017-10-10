f0d3c8931048744643d10d411a625c74ad8f9527
sleahy115 <sleahy115@gmail.com>
2017-10-09 21:46:36 -0600

    create components for links from main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { JoinComponent } from './join/join.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SearchComponent,
+    JoinComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
